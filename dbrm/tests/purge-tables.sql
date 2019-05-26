3eb85128b37a38d75a3a81feb2df6e8035b08958
Geaaru <geaaru@gmail.com>
2019-05-26 19:37:06 +0200

    database: Added purge scripts

---
+++ dbrm/tests/purge-tables.sql
@@ -0,0 +1,20 @@
+SELECT 'Truncate table radacct';
+TRUNCATE TABLE `radacct`;
+
+SELECT 'Truncate table radcheck';
+TRUNCATE TABLE `radcheck`;
+
+SELECT 'Truncate table radgroupcheck';
+TRUNCATE TABLE `radgroupcheck`;
+
+SELECT 'Truncate table radgroupreply';
+TRUNCATE TABLE `radgroupreply`;
+
+SELECT 'Truncate table radreply';
+TRUNCATE TABLE `radreply`;
+
+SELECT 'Truncate table radusergroup';
+TRUNCATE TABLE `radusergroup`;
+
+SELECT 'Truncate table nas';
+TRUNCATE TABLE `nas`;
